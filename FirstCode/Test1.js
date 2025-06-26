aca8ef3e4b7a0c588fd42f2407a77fa43457b8d6
Annu <anne.ploch@gmx.de>
2025-06-26 20:42:41 +0200

    Forgot those oop

---
+++ FirstCode/Test1.js
@@ -0,0 +1,47 @@
+"use strict";
+var Test;
+(function (Test) {
+    console.log("aaaaaa");
+    let x = 1;
+    {
+        // let x:number =0;
+        console.log(x);
+    }
+    console.log(x);
+    // console.log(x) geht nicht weil nicht in Block
+    // In einer Zeile ALT + Pfeiltasten um Zeilen/Auswahl zu verschieben
+})(Test || (Test = {}));
+let v = 1;
+if (v == 1) {
+    console.log("Hello");
+}
+else {
+    console.log("goodbye");
+}
+// 1x "=" ist Deklarierung (Define or Zuweisungoperator/ !assignment operator!), 2x "=" ist Abfrage
+console.log("Switch");
+{
+    let b = 3;
+    switch (b) {
+        case 1:
+            console.log("hello");
+            break;
+        case 2:
+            console.log("goodbye");
+            break;
+        default:
+            console.log("hug");
+            break;
+    }
+}
+var story;
+(function (story) {
+    console.log(parent);
+    console.log(parent.gameState);
+    if (parent.gameState == true) {
+        console.log("you cant get in again");
+        location.href = "owo.html";
+    }
+    parent.gameState = true;
+    console.log(parent.gameState);
+})(story || (story = {}));
---
+++ FirstCode/Test1.ts
@@ -0,0 +1,49 @@
+namespace Test {
+console.log("aaaaaa");
+let x:number =1;
+{
+   // let x:number =0;
+    console.log(x)
+}
+console.log(x)
+// console.log(x) geht nicht weil nicht in Block
+// In einer Zeile ALT + Pfeiltasten um Zeilen/Auswahl zu verschieben
+}
+let v:number =1;
+if (v ==1){
+    console.log("Hello")}
+
+else{
+ console.log("goodbye")}
+// 1x "=" ist Deklarierung (Define or Zuweisungoperator/ !assignment operator!), 2x "=" ist Abfrage
+
+console.log("Switch")
+{
+let b:number=3;
+switch (b) {
+    case 1:
+console.log("hello")
+break;
+    case 2:
+console.log("goodbye")
+break;
+    default:
+console.log("hug")
+break;
+}
+}
+
+namespace story {
+    type Parent = Window & {gameState: boolean};
+    declare let parent: Parent;
+
+    console.log(parent);
+    console.log(parent.gameState);
+    if (parent.gameState == true){
+        console.log("you cant get in again");
+        location.href = "owo.html";
+    
+    }
+    parent.gameState = true;
+    console.log(parent.gameState);
+}
