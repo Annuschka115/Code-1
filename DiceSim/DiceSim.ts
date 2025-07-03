addeaf25fa18b97cfe8e47ffd67f92898457d734
Annu <anne.ploch@gmx.de>
2025-07-03 20:54:23 +0200

    Dice Sim files ye

---
+++ DiceSim/DiceSim.ts
@@ -0,0 +1,7 @@
+/*DiceSim Plan:
+1. Define DiceTypes and properties
+2. Create a Dice class to handle rolling and properties
+3. Implement a DiceSimulator class to manage multiple dice rolls (saving results in an array, include loop for multiple rolls)
+4. Create Output in Console of results (sum, average sum, median and min/max sum)
+*/
+
